4b66a2ad61454821e086d115345a63d61216f77a
aksss7 <aksss7.kg@gmail.com>
2023-05-14 23:18:48 +0600

    sdfsd

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,47 +1,15 @@
-
+import Head from "next/head";
 import "../styles/globals.css";
 
-
-
-
-import React, { useEffect, useState } from 'react';
-import Router from 'next/router';
-import Loader from "@/components/shared/Loader";
-import Head from "next/head";
-
-export default function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const startLoading = () => setLoading(true);
-    const stopLoading = () => setLoading(false);
-
-    Router.events.on('routeChangeStart', startLoading);
-    Router.events.on('routeChangeComplete', stopLoading);
-    Router.events.on('routeChangeError', stopLoading);
-
-    // Завершаем загрузку после первой отрисовки
-    setTimeout(stopLoading, 1000); // Задержка в 1 секунду
-
-    return () => {
-      Router.events.off('routeChangeStart', startLoading);
-      Router.events.off('routeChangeComplete', stopLoading);
-      Router.events.off('routeChangeError', stopLoading);
-    };
-  }, []);
-
+function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      {loading ? (
-        <Loader/> // Ваш лоадер здесь
-      ) : (
-        <>
-          <Head>
-            <base href="/" />
-          </Head>
-          <Component {...pageProps} />
-        </>
-      )}
-    </div>
+    <>
+      <Head>
+        <base href="/" />
+      </Head>
+      <Component {...pageProps} />
+    </>
   );
 }
+
+export default MyApp;
