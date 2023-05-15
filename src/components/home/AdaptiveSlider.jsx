90bcc9e5357db4a3474816cb31bda244801f45ac
aksss7 <aksss7.kg@gmail.com>
2023-05-15 22:29:19 +0600

    sdfww

--- src/components/home/AdaptiveSlider.jsx
+++ src/components/home/AdaptiveSlider.jsx
@@ -1,7 +1,5 @@
 
-
-
-  import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import s from '@/styles/main/AdaptiveSlider.module.css';
 
@@ -11,17 +9,17 @@ const AdaptiveSlider = () => {
     const nextSlide = () => {
         const newIndex = activeIndex === 7 ? 0 : activeIndex + 1;
         setActiveIndex(newIndex);
-    };
+      };
       
     const slideLeft = () => {
         const newIndex = activeIndex === 0 ? 7 : activeIndex - 1;
         setActiveIndex(newIndex);
-    };
+      };
       
     const slideRight = () => {
         const newIndex = activeIndex === 7 ? 0 : activeIndex + 1;
         setActiveIndex(newIndex);
-    };
+      };
   
     const handlers = useSwipeable({
       onSwipedLeft: () => slideRight(),
@@ -29,81 +27,72 @@ const AdaptiveSlider = () => {
     });
 
     const slidesData = [
-      {
-        text: "brief",
-        image: "./assets/images/01.svg"
-      },
-      {
-        text: "business-analysis & creating a CJM",
-        image: "./assets/images/02.svg"
-      },
-      {
-        text: "prototype and its testing (CustDevs)",
-        image: "./assets/images/03.svg"
-      },
-      {
-        text: "brending",
-        image: "./assets/images/04.svg"
-      },
-      {
-        text: "visual concept (ux-ui MVP/MLP)",
-        image: "./assets/images/05.svg"
-      },
-      {
-        text: "development through testing",
-        image: "./assets/images/06.svg"
-      },
-      {
-        text: "correcting MLP & Final launch",
-        image: "./assets/images/07.svg"
-      },
-      {
-        text: "digital-marketing",
-        image: "./assets/images/08.svg"
-      },
-    ];
+        {
+          text: "brief",
+          image: "./assets/images/01.svg"
+        },
+        {
+          text: "business-analysis & creating a CJM",
+          image: "./assets/images/02.svg"
+        },
+        {
+          text: "prototype and its testing (CustDevs)",
+          image: "./assets/images/03.svg"
+        },
+        {
+          text: "brending",
+          image: "./assets/images/04.svg"
+        },
+        {
+          text: "visual concept (ux-ui MVP/MLP)",
+          image: "./assets/images/05.svg"
+        },
+        {
+          text: "development through testing",
+          image: "./assets/images/06.svg"
+        },
+        {
+          text: "correcting MLP & Final launch",
+          image: "./assets/images/07.svg"
+        },
+        {
+          text: "digital-marketing",
+          image: "./assets/images/08.svg"
+        },
+      ];
       
+      
+  
     return (
         <div className={s.sliderWrapper}>
-            <div className={s.sliderContainer} {...handlers}>
-                {slidesData.map((slide, index) => (
-                    <div
-                        key={index}
-                        className={`${s.sliderBlock} ${
-                            index === activeIndex ? s.activeBlock : ""
-                        }`}
-                        onClick={nextSlide}
-                        style={{
-                            zIndex: index === activeIndex ? 2 : 1,
-                            transform:
-                                index === activeIndex
-                                    ? "translateX(0)"
-                                    : index === (activeIndex + 1) % slidesData.length
-                                    ? "translateX(calc(140% + 10px))"
-                                    : index === (activeIndex - 1 + slidesData.length) % slidesData.length
-                                    ? "translateX(calc(-140% - 10px))"
-                                    : "translateX(calc(300% + 10px))",
-                        }}
-                    >
-                        <div className={s.slideContent}>
-                            {index === activeIndex && (
-                                <img src={slide.image} className={s.slideImage} alt={`Slide ${index + 1}`} loading="lazy" />
-                            )}
-                            <div className={s.slideNumber}>{index + 1}</div>
-                        </div>
-                    </div>
-                ))}
-            </div>
+          <div className={s.sliderContainer} {...handlers}>
             {slidesData.map((slide, index) => (
+              index === activeIndex && (
                 <div
-                    key={index}
-                    className={`${s.slideText} ${index === activeIndex ? s.activeText : ""}`}
+                  key={index}
+                  className={`${s.sliderBlock} ${
+                    index === activeIndex ? s.activeBlock : ""
+                  }`}
+                  onClick={nextSlide}
                 >
-                    {slide.text}
+                  <div className={s.slideContent}>
+                    <img src={slide.image} className={s.slideImage} alt={`Slide ${index + 1}`} />
+                    <div className={s.slideNumber}>{index + 1}</div>
+                  </div>
                 </div>
+              )
             ))}
-        </div>    
-        );
-      };
+          </div>
+          {slidesData.map((slide, index) => (
+            <div
+              key={index}
+              className={`${s.slideText} ${index === activeIndex ? s.activeText : ""}`}
+            >
+              {slide.text}
+            </div>
+          ))}
+        </div>
+      );
+};
 
 export default AdaptiveSlider;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,33 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-  render() {
-    return (
-      <Html>
-        <Head></Head>
-        <body>
-          {/* Google Tag Manager (noscript) */}
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQC67DR" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          />
-          {/* Yandex.Metrika noscript */}
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<div><img src="https://mc.yandex.ru/watch/93466519" style="position:absolute; left:-9999px;" alt="" /></div>`,
-            }}
-          />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQC67DR" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* Yandex.Metrika noscript */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<div><img src="https://mc.yandex.ru/watch/93466519" style="position:absolute; left:-9999px;" alt="" /></div>`,
+          }}
+        />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
